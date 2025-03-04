8397e2062856cf82c233414df201b0c1c6c821c7
Kasper Møller <83021349+moller2866@users.noreply.github.com>
2025-03-04 11:16:40 +0000

    changed app name

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/micronaut-app:latest" &
+docker push "ghcr.io/$docker_username/micronaut-app2:1.0-${GIT_COMMIT::8}" 
+docker push "ghcr.io/$docker_username/micronaut-app2:latest" &
 wait
